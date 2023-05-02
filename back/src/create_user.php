bb4ee33b4d70015763803512675e0b0d99b9a5e9
Thor <thorname@gmail.com>
2023-05-02 09:00:58 -0500

    Cambios SRC

--- back/src/create_user.php
+++ back/src/create_user.php
@@ -7,15 +7,19 @@
     $address = $_POST['address'];
     $celPhone = $_POST['cel_phone'];
     $email = $_POST['email'];
-    $pass = $_POST['passwd'];
+    $pass = md5($_POST['passwd']);
 
     $sql = "INSERT INTO USERS (first_name, last_name, number_id, address, cellphone, email, password) VALUES ('$fName', '$lName', '$idNum', '$address', '$celPhone', '$email', '$pass') ";
 
     //$conn -> query($sql);
 
     if($conn -> query($sql) === TRUE){
-        echo " ::: USER HAS BEEN CREATED SUCCESSFULLY ::: ";
+        //echo " ::: USER HAS BEEN CREATED SUCCESSFULLY ::: ";
+        echo "<script> alert('::: USER HAS BEEN CREATED SUCCESSFULLY :::')</script>";
+        header("refresh:0; url=http://127.0.0.1/PETS/front/src/login.html");
     } else{
-        echo " ::: ERROR :" . $conn -> error . "<br>" . $sql ;
+        //echo " ::: ERROR :" . $conn -> error . "<br>" . $sql ;
+        echo "<script> alert('::: USER WASN'T CREATED SUCCESSFULLY :::')</script>";
+        header("refresh:0; url=http://127.0.0.1/PETS/front/src/create_user.html");
     }
 ?>
---
+++ back/src/login.php
@@ -0,0 +1,18 @@
+<?php
+    include("../config/connectionDB.php");
+    // Get data
+    $email = $_POST['email'];
+    $password = md5($_POST['passwd']);
+
+    //Create Query
+    $sql = "SELECT * FROM USERS WHERE email = '$email' AND password = '$password'";
+    $result = $conn -> query ($sql);
+
+    if( $result -> num_rows > 0) {
+        header("Location:http://127.0.0.1/PETS/front/src/home.html");
+    } else{
+        //echo " ::: THE USER DOEN'T EXIST OR INVALID DATA :::" ;
+        echo "<script>alert('::: THE USER DOES NOT EXIST OR INVALID DATA :::') </script>";
+        header("refresh:0; url=http://127.0.0.1/PETS/front/src/login.html");
+    }
+?>
